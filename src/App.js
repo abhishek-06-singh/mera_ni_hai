143f37b99e5320d16c9d0342cba57b4bfa2beca6
Abhishek singh chauahn <115978151+abhishek-06-singh@users.noreply.github.com>
2023-01-19 15:36:10 +0530

    solved

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
+import MovieList from './components/movieList/MovieList';
+import Movie from './pages/movieDetail/Movie';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <Header/>
+        <Routes>
+          <Route index element={<Home/>}></Route>
+          <Route path="/movie/:id" element={<Movie/>}></Route>
+          <Route path="movies/:type" element={<MovieList/>}></Route>
+          <Route path="/*" element={<h1>Error page</h1>}></Route>
+
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
